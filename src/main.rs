ff128eb109ab58c3fd4367522ce01c9106b8d9c3
pulchroxloom <code@dantebarbieri.dev>
2022-08-24 19:44:06 -0700

    Add name validation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
     let mut name = String::new();
     std::io::stdin().read_line(&mut name).expect("Failed to read line.");
+    if name.is_empty() {
+        println!("Not a valid name!");
+        return ;
+    }
     println!("Hello, {}!", name);
 }
